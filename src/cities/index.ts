d111655fbed1f61e0d3f2162fb76abe525ba45b6
Ovidijus Na <ovidijus.na@gmail.com>
2023-10-18 22:24:41 +0300

    new cities

--- src/cities/index.ts
+++ src/cities/index.ts
@@ -1,4 +1,10 @@
-import akmeneImg from './emblems/300px-akmene.png';
+import akmeneImg from './emblems/akmene.png';
+import alytusImg from './emblems/alytus.png';
+import anyksciaiImg from './emblems/anyksciai.png';
+import ariogalaImg from './emblems/ariogala.png';
+import baltoji_vokeImg from './emblems/baltoji_voke.png';
+import birstonasImg from './emblems/birstonas.png';
+import birzaiImg from './emblems/birzai.png';
 
 type Coord = {
     long: number,
@@ -22,18 +28,52 @@ export const cities: City[] = [
     },
     {
         name: 'Alytus',
-        image: 'alytus.png',
+        image: alytusImg,
         location: {
             lang: 54.396,
             long: 24.046
         }
     },
     {
+        // su vainiku 
         name: 'Anykščiai',
-        image: 'anyksciai.png',
+        image: anyksciaiImg,
         location: {
             lang: 55.526,
             long: 25.106
+        }
+    },
+    {
+        name: 'Ariogala',
+        image: ariogalaImg,
+        location: {
+            lang: 55.263,
+            long: 23.483
+        }
+    },
+    {
+        name: 'Baltoji Vokė',
+        image: baltoji_vokeImg,
+        location: {
+            lang: 54.454,
+            long: 25.12
+        }
+    },
+    {
+        name: 'Birštonas',
+        image: birstonasImg,
+        location: {
+            lang: 54.604,
+            long: 24.034
+        }
+    },
+    {
+        // su vainiku 
+        name: 'Biržai',
+        image: birzaiImg,
+        location: {
+            lang: 56.2,
+            long: 24.757
         }
     }
 ]
