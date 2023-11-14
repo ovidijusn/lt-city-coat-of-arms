17d90d97ce3d468d3d7c56779e1326ed89a992e6
Ovidijus Na <ovidijus.na@gmail.com>
2023-11-14 21:09:54 +0200

    open stats on demand

--- vite.config.ts
+++ vite.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
         ]
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff,woff2,ttf,eot}'],
       }
     })
   ],
