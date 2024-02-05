4ba04168f5066d995e28fce0bea66514db9b4313
Ovidijus <ovidijus.na@gmail.com>
2024-02-05 19:56:17 +0200

    Update CoaGameHelp.vue
    grama

--- src/components/CoaGameHelp.vue
+++ src/components/CoaGameHelp.vue
@@ -12,16 +12,16 @@
             Kiekvieną dieną atsitiktinė tvarka bus išrenkamas Lietuvos miesto herbas(išskyrus <a
               href="https://www.vle.lt/straipsnis/panemune/" target="_blank">Panemunę</a>, kuri iki šiol
             neturi
-            herbo) ir žaidimo tikslas - atspėti kokio, tai miesto herbas su kuo mažiau spėjimų. Visi žaidėjai tą pačia
+            herbo) ir žaidimo tikslas - atspėti, kokio tai miesto herbas su kuo mažiau spėjimų. Visi žaidėjai tą pačią
             dieną
             gauna tą patį herbą.
           </p>
           <p class="text-body-2 ma-4">Žaidimas prasideda su viena atversta herbo dalimi iš 6 galimų.</p>
           <p class="text-body-2 ma-4"> Po kiekvieno blogo spėjimo atverčiama nauja herbo dalis bei pateikiamas apytikslis
-            atstumas ir kriptis nuo spėto
+            atstumas ir kryptis nuo spėto
             miesto iki tos dienos miesto.</p>
-          <p class="text-body-2 ma-4">Atspėjus atverčiamos visos užverstos herbo dalis bei pateikiama asmeninė žaidimo
-            statistika, duomenis apie
+          <p class="text-body-2 ma-4">Atspėjus atverčiamos visos užverstos herbo dalys bei pateikiama asmeninė žaidimo
+            statistika, duomenys apie
             praeities žaidimus saugomi naršyklėje.</p>
           <p class="text-body-2 ma-4"> Neatspėjus bus pateikiama žaidimų statistika su tos dienos miesto pavadinimu.
           </p>
