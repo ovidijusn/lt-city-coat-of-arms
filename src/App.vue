f098cf2e6b1da7133a2a2dacfd3ac55d29ddcfdc
Ovidijus Na <ovidijus.na@gmail.com>
2024-02-13 20:49:00 +0200

    reload on new day

--- src/App.vue
+++ src/App.vue
@@ -12,11 +12,24 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted } from 'vue';
 import CoaGameCard from './components/CoaGameCard.vue';
 import CoaDebugCard from './components/CoaDebugCard.vue';
+import { seed, genSeed } from './utils/random';
 
 
 const isDev = 'development' === import.meta.env.MODE;
+
+onMounted(() => {
+    document.addEventListener('visibilitychange', () => {
+        if (!document.hidden) {
+            const [newSeed] = genSeed();
+            if (newSeed !== seed) {
+                window.location.reload();
+            }
+        }
+    });
+});
 </script>
 
 <style scoped></style>
